4468b89913ece11e0f1d38a790b896c772e91ca5
Jagielski, Michał <michal77_1996@onet.eu>
2018-08-07 17:43:23 +0200

    Add basic menu
    +main menu
    +options menu

---
+++ Android_Game/Assets/Scenes/MainMenu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGameButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGameButton()
+    {
+        Debug.Log("QUIT");
+    }
+}

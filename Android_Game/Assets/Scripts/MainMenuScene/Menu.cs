b7106179ee460f8975e92220336dffd9bbe413c0
Jagielski, Michał <michal77_1996@onet.eu>
2018-08-15 16:23:25 +0200

    Change file directions
    +change files
    +resize screen
    +add basic folders

---
+++ Android_Game/Assets/Scripts/MainMenuScene/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Menu : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    public void Start()
+    {
+        this.audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnOffAudio()
+    {
+        if (this.audioSource.mute == true)
+            this.audioSource.mute = false;
+        else
+            this.audioSource.mute = true;
+    }
+}
---
+++ Android_Game/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+}
